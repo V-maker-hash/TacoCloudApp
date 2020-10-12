48b269dffa295d5b24126777ed8b633fa39c0c2b
Vasily <leonidasway11051989@gmail.com>
2020-10-12 22:29:00 +0300

    ADD JPA Annotations to the Ingredient fields

--- src/main/java/com/example/tacocloud/domain/Ingredient.java
+++ src/main/java/com/example/tacocloud/domain/Ingredient.java
@@ -1,17 +1,29 @@
 package com.example.tacocloud.domain;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
+@Entity
 public class Ingredient {
 
+    @Id
     private final String id;
+
     private final String name;
+
+    @Enumerated(value = EnumType.STRING)
     private final Type type;
 
     public static List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
