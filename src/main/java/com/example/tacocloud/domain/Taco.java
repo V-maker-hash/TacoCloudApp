f14d09f29bbf99996f3d3859ed03366ba12022fb
Vasily <leonidasway11051989@gmail.com>
2020-10-12 22:29:52 +0300

    ADD JPA Annotations to the Taco fields

--- src/main/java/com/example/tacocloud/domain/Taco.java
+++ src/main/java/com/example/tacocloud/domain/Taco.java
@@ -1,7 +1,9 @@
 package com.example.tacocloud.domain;
 
 import lombok.Data;
+import org.hibernate.annotations.GeneratorType;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
@@ -9,9 +11,13 @@ import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
 
     private Date createdAt;
 
@@ -19,6 +25,12 @@ public class Taco {
     @Size(min = 5, message = "Name must be at least 4 characters long")
     private String name;
 
+    @ManyToMany(targetEntity = Ingredient.class)
     @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients = new ArrayList<>();
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
 }
