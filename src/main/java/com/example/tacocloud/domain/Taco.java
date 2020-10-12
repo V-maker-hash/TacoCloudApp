0df3665029f50dba7f538e5b75a5d136756a7586
Vasily <leonidasway11051989@gmail.com>
2020-10-12 10:24:31 +0300

    saving Taco in JDBCTacoRepository

--- src/main/java/com/example/tacocloud/domain/Taco.java
+++ src/main/java/com/example/tacocloud/domain/Taco.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -19,5 +19,5 @@ public class Taco {
     private String name;
 
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<String> ingredients;
+    private List<Ingredient> ingredients;
 }
