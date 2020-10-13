5cb98b170c0b272d3e0dde73e31994134d099fd7
Vasily <leonidasway11051989@gmail.com>
2020-10-13 17:27:10 +0300

    Defining a custom user details service

---
+++ src/main/java/com/example/tacocloud/security/UserRepositoryUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.tacocloud.security;
+
+import com.example.tacocloud.domain.User;
+import com.example.tacocloud.jpaRepositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserRepositoryUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserRepositoryUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user != null) {
+            return user;
+        }
+        throw new UsernameNotFoundException(
+                "User '" + username + "' not found");
+    }
+}
