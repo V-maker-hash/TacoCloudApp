13801e923bbfd5867a5b6a6b76d9d7e812603254
Vasily <leonidasway11051989@gmail.com>
2020-10-14 13:46:58 +0300

    delete JDBC

--- src/main/java/com/example/tacocloud/jdbcrepositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tacocloud.jdbcrepositories;
-
-import com.example.tacocloud.domain.Order;
-
-public interface OrderRepository {
-    Order save(Order order);
-}
--- src/main/java/com/example/tacocloud/jdbcrepositories/TacoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tacocloud.jdbcrepositories;
-
-import com.example.tacocloud.domain.Taco;
-
-public interface TacoRepository {
-    Taco save(Taco design);
-}
